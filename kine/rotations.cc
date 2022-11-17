38b163ee66f58abcde4c86e1e74706d68742b5fc
Colin Egerer <cpegerer@email.wm.edu>
2022-11-17 16:10:53 -0500

    Implement correct representations of rotations for spin-1/2 states

---
+++ kine/rotations.cc
@@ -0,0 +1,43 @@
+/*
+  Establish Euler rotations
+*/
+#include<iostream>
+#include "rotations.h"
+#include<math.h>
+
+namespace Rotations
+{
+  // Rotation matrix for spin = 1/2 vectors using Euler angles in zyz-convention
+  /*
+    U[R(a,b,g)] = exp(-i a/2 \sigma_z) * exp(-i b/2 \sigma_y) * exp(-i g/2 \sigma_z)
+  */
+  gmc * eulerRotMat2(double alpha, double beta, double gamma)
+  {    
+    gmc * Rza = gsl_matrix_complex_calloc(2,2);
+    gsl_matrix_complex_set(Rza,0,0,gc_rect(cos(alpha/2),-sin(alpha/2)));
+    gsl_matrix_complex_set(Rza,1,1,gc_rect(cos(alpha/2),sin(alpha/2)));
+
+    gmc * Ryb = gsl_matrix_complex_calloc(2,2);
+    gsl_matrix_complex_set(Ryb,0,0,gc_rect(cos(beta/2),0));
+    gsl_matrix_complex_set(Ryb,0,1,gc_rect(-sin(beta/2),0));
+    gsl_matrix_complex_set(Ryb,1,0,gc_rect(sin(beta/2),0));
+    gsl_matrix_complex_set(Ryb,1,1,gc_rect(cos(beta/2),0));
+
+    gmc * Rzg = gsl_matrix_complex_calloc(2,2);
+    gsl_matrix_complex_set(Rzg,0,0,gc_rect(cos(gamma/2),-sin(gamma/2)));
+    gsl_matrix_complex_set(Rzg,1,1,gc_rect(cos(gamma/2),sin(gamma/2)));
+
+    gmc * mat = gsl_matrix_complex_calloc(2,2);
+    gmc * res = gsl_matrix_complex_calloc(2,2);
+    
+    gsl_blas_zgemm(CblasNoTrans,CblasNoTrans,one,Rza,Ryb,zero,mat);
+    gsl_blas_zgemm(CblasNoTrans,CblasNoTrans,one,mat,Rzg,zero,res);
+
+    gsl_matrix_complex_free(Rza);
+    gsl_matrix_complex_free(Ryb);
+    gsl_matrix_complex_free(Rzg);
+    gsl_matrix_complex_free(mat);
+
+    return res;
+  }
+}
---
+++ kine/rotations.h
@@ -0,0 +1,25 @@
+/*
+  Establish Euler rotations
+*/
+#ifndef _rotations_h_
+#define _rotations_h_
+
+#include<gsl/gsl_blas.h>
+#include<gsl/gsl_linalg.h>
+#include<gsl/gsl_matrix.h>
+#include<gsl/gsl_vector.h>
+#include<gsl/gsl_complex.h>
+#include<gsl/gsl_complex_math.h>
+
+#include "shortcuts_gsl.h"
+
+namespace Rotations
+{
+  // Rotation matrix for spin = 1/2 vectors using Euler angles in zyz-convention
+  /*
+    U[R(a,b,g)] = exp(-i a/2 \sigma_z) * exp(-i b/2 \sigma_y) * exp(-i g/2 \sigma_z)
+  */
+  gmc * eulerRotMat2(double alpha, double beta, double gamma);  
+}
+
+#endif
